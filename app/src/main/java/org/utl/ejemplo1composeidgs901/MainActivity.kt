b116dd394ace9616cdc5b815f4c7f52681e5236e
IDGS-901-22002224 <carlosraullpz@gmail.com>
2025-06-18 18:35:04 -0600

    "Tarjetas Terminadas 18/06/2025"

--- app/src/main/java/org/utl/ejemplo1composeidgs901/MainActivity.kt
+++ app/src/main/java/org/utl/ejemplo1composeidgs901/MainActivity.kt
@@ -1,40 +1,51 @@
-package org.utl.ejemplo1composeidgs901
+package org.utl.idgs.ejemplo1composeidgs901
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+data class PersonajeTarjeta(val nombre: String, val descripcion: String)
 
-private val Tarjetas: List<PersonajeTarjeta> = listOf(
-    PersonajeTarjeta("Goku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador."),
-    PersonajeTarjeta("Vegeta", "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie."),
-    PersonajeTarjeta("Piccolo", "Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación. Aunque en un principio fue el archienemigo de Son Goku, con el paso del tiempo fue haciéndose menos malvado hasta finalmente convertirse en un ser bondadoso y miembro de los Guerreros Z. A través del tiempo, también comenzó a tomarle cariño a su discípulo Son Gohan, a quien veía como una especie de \\\"vástago\\\" y formando un lazo de amistad con este."),
-    PersonajeTarjeta("Freezer","Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
-    PersonajeTarjeta("Zarbon", "Zarbon es uno de los secuaces de Freezer y un luchador poderoso."),
-    PersonajeTarjeta("Dodoria", "Dodoria es otro secuaz de Freezer conocido por su brutalidad."),
-    PersonajeTarjeta("Gohan", "Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre saiyano y humano terrícola. Es el primer hijo de Son Goku y Chi-Chi, hermano mayor de Son Goten, esposo de Videl y padre de Pan."),
-    PersonajeTarjeta("Krillin", "Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku. Es junto a Bulma uno de los personajes de apoyo principales de Dragon Ball, Dragon Ball Z y Dragon Ball Super. Aparece en Dragon Ball GT como personaje secundario. En el Arco de Majin Boo, se retira de las artes marciales, optando por formar una familia, como el esposo de la Androide Número 18 y el padre de Marron."),
-    PersonajeTarjeta("Tenshinhan", "Maestro de las artes marciales y miembro de los Z Fighters.  Es un personaje que aparece en el manga y en el anime de Dragon Ball, Dragon Ball Z, Dragon Ball GT y posteriormente en Dragon Ball Super.")
+private val tarjetas: List<PersonajeTarjeta> = listOf(
+    PersonajeTarjeta("Koku", "El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla. Sin embargo, el caer por un barranco le proporcionó un brutal golpe que si bien casi lo mata, este alteró su memoria y anuló todos los instintos violentos de su especie, lo que lo hizo crecer con un corazón puro y bondadoso, pero conservando todos los poderes de su raza. No obstante, en la nueva continuidad de Dragon Ball se establece que él fue enviado por sus padres a la Tierra con el objetivo de sobrevivir a toda costa a la destrucción de su planeta por parte de Freeza. Más tarde, Kakarot, ahora conocido como Son Goku, se convertiría en el príncipe consorte del monte Fry-pan y líder de los Guerreros Z, así como el mayor defensor de la Tierra y del Universo 7, logrando mantenerlos a salvo de la destrucción en innumerables ocasiones, a pesar de no considerarse a sí mismo como un héroe o salvador."),
+    PersonajeTarjeta("vegeta_normal", "Príncipe de los Saiyans, inicialmente un villano, pero luego se une a los Z Fighters. A pesar de que a inicios de Dragon Ball Z, Vegeta cumple un papel antagónico, poco después decide rebelarse ante el Imperio de Freeza, volviéndose un aliado clave para los Guerreros Z. Con el paso del tiempo llegaría a cambiar su manera de ser, optando por permanecer y vivir en la Tierra para luchar a su lado contra las inminentes adversidades que superar. Junto con Piccolo, él es de los antiguos enemigos de Goku que ha evolucionando al pasar de ser un villano y antihéroe, a finalmente un héroe a lo largo del transcurso de la historia, convirtiéndose así en el deuteragonista de la serie."),
+    PersonajeTarjeta("picolo_normal", "Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación. Aunque en un principio fue el archienemigo de Son Goku, con el paso del tiempo fue haciéndose menos malvado hasta finalmente convertirse en un ser bondadoso y miembro de los Guerreros Z. A través del tiempo, también comenzó a tomarle cariño a su discípulo Son Gohan, a quien veía como una especie de \\\"vástago\\\" y formando un lazo de amistad con este."),
+    PersonajeTarjeta("freezer","Freezer es el tirano espacial y el principal antagonista de la saga de Freezer."),
+    PersonajeTarjeta("zarbon", "Zarbon es uno de los secuaces de Freezer y un luchador poderoso."),
+    PersonajeTarjeta("dodoria", "Dodoria es otro secuaz de Freezer conocido por su brutalidad."),
+    PersonajeTarjeta("gohan", "Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre saiyano y humano terrícola. Es el primer hijo de Son Goku y Chi-Chi, hermano mayor de Son Goten, esposo de Videl y padre de Pan."),
+    PersonajeTarjeta("krilin_universo7", "Amigo cercano de Goku y guerrero valiente, es un personaje del manga y anime de Dragon Ball. Es uno de los principales discípulos de Kame-Sen'nin, Guerrero Z, y el mejor amigo de Son Goku. Es junto a Bulma uno de los personajes de apoyo principales de Dragon Ball, Dragon Ball Z y Dragon Ball Super. Aparece en Dragon Ball GT como personaje secundario. En el Arco de Majin Boo, se retira de las artes marciales, optando por formar una familia, como el esposo de la Androide Número 18 y el padre de Marron."),
+    PersonajeTarjeta("tenshinhan_universo7", "Maestro de las artes marciales y miembro de los Z Fighters.  Es un personaje que aparece en el manga y en el anime de Dragon Ball, Dragon Ball Z, Dragon Ball GT y posteriormente en Dragon Ball Super.")
 
 )
 
@@ -45,89 +56,98 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            Ejemplo1ComposeIDGS901Theme {
-                SaludoCard("Android", "Imagen de Carga")
-                Tarjeta(tarjetas)
-            }
+            Ejemplo1ComposeIDGS901Theme { Tarjeta(tarjetas) }
 
         }
     }
 }
 
 @Composable
-fun Tarjeta(personajes: List<PersonajeTarjeta>) {
-    LazyColumn {
-        items (personajes) { personaje ->
-            Mypersonajes(personaje)
+fun SaludoCard(name: String, datosCard: String){
+    Row() {
+        Column {
+            Text("Nombre: $name")
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text("Personaje: $datosCard")
         }
     }
 }
-
-@Composable
-fun Mypersonajes(personaje: PersonajeTarjeta) {
-    Row (){
-        ImagenPersonaje(personaje.nombre.lowercase())
-        Personajes(personaje)
-
-    }
-}
-
-@Composable
-fun Personajes(personaje: PersonajeTarjeta){
-    Column {
-        Personajes(personaje.nombre)
-        Personajes(personaje.descripcion)
-    }
-
-
-@Composable
-fun Personaje(datos:String){
-    Text(datos)
-}
-
-@Composable
-fun ImagenPersonaje(name: String){
-    Image(
-        painter = painterResource(id = R.drawable.dragon),
-        contentDescription = "Android Logo",
-        modifier = Modifier.padding(8.dp)
-            .size(100.dp)
-            .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.error)
-    )
-}
-
-
-@Composable
-fun SaludoCard(name: String, datsoCard: String){
-    Row (){
-    Image(
-        painter = painterResource(id = R.drawable.dragon),
-        contentDescription = "Android Logo",
-        modifier = Modifier.padding(8.dp)
-            .size(100.dp)
-            .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.error)
-    )
-
-    Column {
-        Text("Nombre $name",
-
-        )
-        Spacer(modifier = Modifier.padding(8.dp))
-        Text("DatosCard $datsoCard ")
-
-    }
-
-    }
-}
-
-
 
 @Preview
 @Composable
-fun PreviewSaludoCard() {
+fun PreviewSaludoCard(){
     Column {
-        SaludoCard("Android", "Imagen de Carga")
+        SaludoCard("Oscar", "personaje")
     }
+}
+
+@Composable
+fun Tarjeta(personajes: List<PersonajeTarjeta>){
+    LazyColumn {
+        items(personajes) { personaje ->
+            MyPersonaje(personaje)
+        }
+    }
+}
+
+@Composable
+fun MyPersonaje(personaje: PersonajeTarjeta){
+    Card (
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Row(
+            modifier = Modifier.padding(8.dp)
+                .background(MaterialTheme.colorScheme.background)
+        ) {
+            ImagenPersonaje(personaje.nombre)
+            Personajes(personaje)
+        }
+    }
+}
+
+@Composable
+fun Personaje(name: String, color: Color, style: TextStyle, lines: Int=Int.MAX_VALUE){
+    Text(text = name, color = color, style = style, maxLines = lines)
+}
+
+@Composable
+fun Personajes(personaje: PersonajeTarjeta) {
+    var expanded by remember { mutableStateOf(false) }
+    Column (
+        modifier = Modifier.padding(start = 8.dp).clickable {
+            expanded = !expanded
+        }
+    ) {
+        Personaje(personaje.nombre,
+            MaterialTheme.colorScheme.primary,
+            MaterialTheme.typography.headlineMedium)
+
+        Personaje(personaje.descripcion,
+            MaterialTheme.colorScheme.onBackground,
+            MaterialTheme.typography.bodyLarge,
+            if (expanded) Int.MAX_VALUE else 1
+        )
+    }
+}
+
+@Composable
+fun ImagenPersonaje(imageName: String) {
+    val context = LocalContext.current
+    val ImegeResId = remember(imageName) {
+        context.resources.getIdentifier(imageName.lowercase(),
+            "drawable", context.packageName)
+    }
+    Image(
+        painter = painterResource(id= ImegeResId),
+        contentDescription = null,
+        modifier = Modifier.padding(16.dp)
+            .size(100.dp)
+            .clip(CircleShape)
+            .background(MaterialTheme.colorScheme.tertiary)
+    )
 }
